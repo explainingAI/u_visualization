9a7b850dfe5562252d152827cad68a9ade049f17
JhonierMeneses15 <jhoniermeneses2001@gmail.com>
2022-08-14 23:36:36 +0200

    dev add pca and tsne train and plot data

--- main/app.py
+++ main/app.py
@@ -1,21 +1,61 @@
 import matplotlib.pyplot as plt
-import  pandas as pd
-from  matplotlib import pyplot
-from  sklearn.decomposition import  PCA
-import numpy as np
+import pandas as pd
+from sklearn.decomposition import  PCA
 from sklearn.preprocessing import StandardScaler
+from sklearn.manifold import TSNE
+from tkinter import filedialog
+import logging as log
 
-data = pd.read_csv("/home/jhonier/Documentos/Colaboracion/u_visualization/data/data.csv")
+def openfilename():
+    # The dialogue box has a title "Open"
+    filename = filedialog.askopenfilename(title="Open csv")
+    return filename
 
-X_std = StandardScaler().fit_transform(data)
+def trainPca(data):
+    nuevo_pca = PCA(n_components=2)
+    data_pca = nuevo_pca.fit_transform(data)
+    return data_pca
 
-nuevo_pca = PCA(n_components=2)
-X = nuevo_pca.fit_transform(X_std)
-print(X)
-data2d = pd.DataFrame(X)
+def trainTsne(data):
+    x_embedded = TSNE(n_components=2, learning_rate='auto',
+    init ='random', perplexity = 3).fit_transform(data)
 
-x = data2d.iloc[:, 0] # Primera columna
-y = data2d.iloc[:, 1] # Segunda columna
+    return x_embedded
 
-plt.scatter(x,y)
-plt.show()
+def main():
+    path= openfilename()
+    #path="/home/jhonier/Documentos/Colaboracion/u_visualization/data/data.csv"
+    log.info("read dataset: ", path)
+    data = pd.read_csv(path)
+
+    x_std = StandardScaler().fit_transform(data)
+
+    log.info("train data with PCA")
+    #PCA
+    data_pca= trainPca(x_std)
+    dfPca = pd.DataFrame(data_pca)
+    x_pca = dfPca.iloc[:, 0]
+    y_pca = dfPca.iloc[:, 1]
+
+    log.info("train data with TSNE")
+    #TSNE
+    data_tsne = trainTsne(x_std)
+    data_tsne = pd.DataFrame(data_tsne)
+    x_tsne = data_tsne.iloc[:, 0]  # Primera columna
+    y_tsne = data_tsne.iloc[:, 1]  # Segunda columna
+
+    log.info("plotting data")
+    #plot
+    fig,(pltPca,pltTsne) = plt.subplots(2)
+
+    fig.suptitle('Data visualization')
+
+    pltPca.set_title("pca")
+    pltPca.scatter(x_pca, y_pca)
+
+    pltTsne.set_title("tsne")
+    pltTsne.scatter(x_tsne, y_tsne)
+    plt.show()
+
+if __name__ == '__main__':
+    main()
